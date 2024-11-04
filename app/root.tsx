ac85c881603b435a9a1b8bf16a5e34e996038cac
Christof Kälin <christof.kaelin@protonmail.com>
2024-11-04 20:08:03 +0100

    updates

--- app/root.tsx
+++ app/root.tsx
@@ -1,12 +1,54 @@
 import {
   Form,
   Links,
+  NavLink,
   Meta,
+  Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
+  useNavigation,
+  useSubmit,
 } from "@remix-run/react";
+import { json, redirect } from "@remix-run/node";
+
+import type { LinksFunction, LoaderFunctionArgs } from "@remix-run/node";
+import appStylesHref from "./app.css?url";
+
+import { createEmptyContact, getContacts } from "./data";
+import { useEffect } from "react";
+
+export const action = async () => {
+  const contact = await createEmptyContact();
+  return redirect(`/contacts/${contact.id}/edit`);
+};
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: appStylesHref },
+];
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const url = new URL(request.url);
+  const search = url.searchParams.get("q");
+  const contacts = await getContacts(search);
+  return json({ contacts, search });
+};
 
 export default function App() {
+  const { contacts, search } = useLoaderData<typeof loader>();
+  const navigation = useNavigation();
+  const submit = useSubmit();
+  const isSearching =
+    navigation.location &&
+    new URLSearchParams(navigation.location.search).has("q");
+
+  useEffect(() => {
+    const searchField = document.getElementById("q");
+    if (searchField instanceof HTMLInputElement) {
+      searchField.value = search ?? "";
+    }
+  }, [search]);
+
   return (
     <html lang="en">
       <head>
@@ -19,30 +61,63 @@ export default function App() {
         <div id="sidebar">
           <h1>Remix Contacts</h1>
           <div>
-            <Form id="search-form" role="search">
+            <Form
+              id="search-form"
+              role="search"
+              onChange={(event) => {
+                const isFirstSearch = search === null;
+                submit(event.currentTarget, { replace: !isFirstSearch });
+              }}>
               <input
+                className={isSearching ? "loading" : ""}
                 id="q"
+                defaultValue={search ?? ""}
                 aria-label="Search contacts"
                 placeholder="Search"
                 type="search"
                 name="q"
               />
-              <div id="search-spinner" aria-hidden hidden={true} />
+              <div id="search-spinner" aria-hidden hidden={!isSearching} />
             </Form>
             <Form method="post">
               <button type="submit">New</button>
             </Form>
           </div>
           <nav>
-            <ul>
-              <li>
-                <a href={`/contacts/1`}>Your Name</a>
-              </li>
-              <li>
-                <a href={`/contacts/2`}>Your Friend</a>
-              </li>
-            </ul>
+            {contacts.length ? (
+              <ul>
+                {contacts.map((contact) => (
+                  <li key={contact.id}>
+                    <NavLink
+                      className={({ isActive, isPending }) =>
+                        isActive ? "active" : isPending ? "pending" : ""
+                      }
+                      to={`contacts/${contact.id}`}>
+                      {contact.first || contact.last ? (
+                        <>
+                          {contact.first} {contact.last}
+                        </>
+                      ) : (
+                        <i>No Name</i>
+                      )}{" "}
+                      {contact.favorite ? <span>★</span> : null}
+                    </NavLink>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p>
+                <i>No contacts</i>
+              </p>
+            )}
           </nav>
+        </div>
+        <div
+          id="detail"
+          className={
+            navigation.state === "loading" && !isSearching ? "loading" : ""
+          }>
+          <Outlet />
         </div>
 
         <ScrollRestoration />
